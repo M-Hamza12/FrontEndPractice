a1e7a7e2526b5b8fff585c61b56be8d3ddd0d281
M-Hamza12 <123285935+M-Hamza12@users.noreply.github.com>
2023-04-13 17:03:41 +0500

    added another tailwind project

---
+++ expenses-chart-component-main/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      softRed: "hsl(10, 79%, 65%)",
+      cyan: "hsl(186, 34%, 60%)",
+      paleOrange: "hsl(33, 100%, 98%)",
+      cream: "hsl(27, 66%, 92%)",
+      mediumBrown: "hsl(28, 10%, 53%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
